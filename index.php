952dd8b8208c75ac15d306f2f73edbc8ef36c88a
Aldemir Junior <159361439+AldemirJunior1@users.noreply.github.com>
2024-10-30 00:03:32 +0000

    Alteração

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=<inp, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
@@ -11,36 +11,37 @@
     <pre>
     <label>Numero 01</label>
     <input type="text" name="numero1" id="numero1"/>
-    
+
     <label>Numero 02</label>
     <input type="text" name="numero2" id="numero2"/>
 
-            <button onclick ="calcular">Calcular com ajax</button>
-    <p id ="resultado"></p>
+    <button onclick="calcular();">Calcular com ajax</button>
+
+    <p id="resultado"></p>
     </pre>
     <script>
         function calcular(){
             const numero1 = document.getElementById("numero1").value;
             const numero2 = document.getElementById("numero2").value;
 
-            fetch('/calculo.php',{
+            fetch('/calculo.php', {
                 method: 'POST',
-                headers:{' Content-Type': 'application/json'},
+                headers: { 'Content-Type': 'application/json'},
                 body: JSON.stringify({
                     numero1: parseFloat(numero1),
-                    numero2: parseFloat(numero2)
-                })    
+                    numero2: parseFloat(numero2)                
+                })
             })
             .then(resposta => resposta.json())
             .then(dados =>{
-                
+
                 document.getElementById("resultado").innerHTML = 
-                "Soma:" + dados.soma;
+                    "Soma: " + dados.soma;
             })
-            .catch(erro=> {
+            .catch(erro =>{
                 document.getElementById("resultado").innerHTML = 
                 "Erro ao processar";
-            })
+            });
         }
     </script>
 </body>
